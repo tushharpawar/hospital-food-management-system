34663ecab7bcb6765a7b20a15bd8a6a1ea6034f6
Tushar Pawar <tushharpawar14@gmail.com>
2025-01-10 21:01:31 +0530

    Create route.ts

---
+++ src/app/api/get-diet-of-patient/[patientId]/route.ts
@@ -0,0 +1,55 @@
+import {  NextRequest, NextResponse } from "next/server";
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const GET = async (req:NextRequest,{params}:{params:{patientId:string}}) => {
+
+  try {
+    const patientId = await params.patientId
+
+    const dietCharts = await prisma.dietChart.findMany({
+        where:{
+            patientId
+        },
+        include:{
+            PantryTask:true,
+        }
+    });
+
+    if(!dietCharts){
+        return NextResponse.json(
+            {
+              success: false,
+              message: "There are no diet chart",
+            },
+            { status: 401 }
+          );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Diet charts fetched successfully!",
+        data: dietCharts,
+      },
+      { status: 201 }
+    );
+  } catch (error: unknown) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.error("Prisma error occurred:", error.message);
+    } else if (error instanceof Error) {
+      console.error("An error occurred:", error.message);
+    } else {
+      console.error("An unknown error occurred:", error);
+    }
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Failed to get dietCharts",
+      },
+      { status: 500 }
+    );
+  }
+};
