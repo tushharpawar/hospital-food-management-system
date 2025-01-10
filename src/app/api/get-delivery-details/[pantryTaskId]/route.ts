6e0aa624f05e2a738ed6bcaefb654dcde09a6563
Tushar Pawar <tushharpawar14@gmail.com>
2025-01-10 21:01:25 +0530

    Create route.ts

---
+++ src/app/api/get-delivery-details/[pantryTaskId]/route.ts
@@ -0,0 +1,55 @@
+import {  NextRequest, NextResponse } from "next/server";
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const GET = async (req:NextRequest,{params}:{params:{pantryTaskId:string}}) => {
+
+  try {
+    const pantryTaskId = await params.pantryTaskId
+
+    const deliveryDetails = await prisma.pantryTask.findFirst({
+        where:{
+            id:pantryTaskId
+        },
+        include:{
+            DeliveryTask:true
+        }
+    });
+
+    if(!deliveryDetails){
+        return NextResponse.json(
+            {
+              success: false,
+              message: "There is not any delivery details",
+            },
+            { status: 401 }
+          );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "deliveryDetails fetched successfully!",
+        data: deliveryDetails,
+      },
+      { status: 201 }
+    );
+  } catch (error: unknown) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.error("Prisma error occurred:", error.message);
+    } else if (error instanceof Error) {
+      console.error("An error occurred:", error.message);
+    } else {
+      console.error("An unknown error occurred:", error);
+    }
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Failed to fetch delivery details",
+      },
+      { status: 500 }
+    );
+  }
+};
