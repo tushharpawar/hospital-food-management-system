a60dcc6ecf3881812d0a15a7195dc826965af187
Tushar Pawar <tushharpawar14@gmail.com>
2025-01-10 21:01:28 +0530

    Create route.ts

---
+++ src/app/api/get-delivery-task/route.ts
@@ -0,0 +1,47 @@
+import {  NextResponse } from "next/server";
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const GET = async () => {
+  try {
+
+    const deliveryTask = await prisma.deliveryTask.findMany();
+
+    if(!deliveryTask){
+        return NextResponse.json(
+            {
+              success: false,
+              message: "There is no Delivery Task",
+              data: deliveryTask,
+            },
+            { status: 404 }
+          );
+    }
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Delivery Task fetched successfully!",
+        data: deliveryTask,
+      },
+      { status: 201 }
+    );
+  } catch (error: unknown) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.error("Prisma error occurred:", error.message);
+    } else if (error instanceof Error) {
+      console.error("An error occurred:", error.message);
+    } else {
+      console.error("An unknown error occurred:", error);
+    }
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Failed to get Delivery Task",
+      },
+      { status: 500 }
+    );
+  }
+};
