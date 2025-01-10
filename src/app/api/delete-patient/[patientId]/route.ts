9770489c61272e23bee447b94d99442ddb25245c
Tushar Pawar <tushharpawar14@gmail.com>
2025-01-10 21:01:17 +0530

    Create route.ts

---
+++ src/app/api/delete-patient/[patientId]/route.ts
@@ -0,0 +1,71 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient, Prisma } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const POST = async (req: NextRequest,{params}:{params:{patientId:string}}) => {
+  try {
+    const {patientId} = await params
+
+    const deletePatientDetails = await prisma.patient.delete({
+        where:{
+            id:patientId
+        }
+    })
+
+    if(deletePatientDetails){
+
+        await prisma.dietChart.deleteMany({
+            where:{
+                patientId
+            }
+        })
+
+        const dietChart = await prisma.dietChart.findMany({
+            where:{patientId}
+        })
+
+        await Promise.all(
+            dietChart.map(async (chart) => {
+                await prisma.pantryTask.deleteMany({
+                    where: {
+                        dietChartId: chart.id,
+                    },
+                });
+            })
+        );
+
+        return NextResponse.json(
+            {
+              success: true,
+              message: "Patient details deleted",
+            },
+            { status: 202 }
+          );
+    }else{
+        return NextResponse.json(
+            {
+              success: true,
+              message: "Failed to delete patient details",
+            },
+            { status: 501 }
+          );
+    } 
+  } catch (error: unknown) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      console.error("Prisma error occurred:", error.message);
+    } else if (error instanceof Error) {
+      console.error("Error occurred:", error.message);
+    } else {
+      console.error("Something went wrong:", error);
+    }
+
+    return NextResponse.json(
+      {
+        success: false,
+        message: "Failed to delete patient details",
+      },
+      { status: 500 }
+    );
+  }
+};
